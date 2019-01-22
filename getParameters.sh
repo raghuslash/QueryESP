9d56101a5aa385703928f8238bddea2dda831f03
Raghunath <raghu@caramail.com>
2019-01-22 18:17:50 +0530

    autocommit by raghu

--- getParameters.sh
+++ getParameters.sh
@@ -3,7 +3,7 @@
 sh runEventQ.sh queries/loader1Q.json
 t=$(cat temp | jq '.aggregations.line_loader_delay.value')
 t=$(bc -l <<< "scale=1; $t/1")
-echo "line_loader.delay: $t" > DTparameters.txt
+echo "line_loader.delay=$t" > DTparameters.txt
 
 
 sh runEventQ.sh queries/screenprinter0Q.json
@@ -12,7 +12,7 @@ e=$(cat temp | jq '.aggregations.screen_printer_idle_energy.value')
 t=$(bc -l <<< "scale=1; $t/1")
 e=$(bc -l <<< "scale=1; $e/1")
 p0=$(bc -l <<< "scale=0;`echo $e` * 3600000 / `echo $t`")
-#echo "screen_printer.idle_delay: $t" >> DTparameters.txt
+#echo "screen_printer.idle_delay=$t" >> DTparameters.txt
 
 
 sh runEventQ.sh queries/screenprinter1Q.json
@@ -21,7 +21,7 @@ e=$(cat temp | jq '.aggregations.screen_printer_printing_energy.value')
 t=$(bc -l <<< "scale=1; $t/1")
 e=$(bc -l <<< "scale=1; $e/1")
 p1=$(bc -l <<< "scale=0;`echo $e` * 3600000 / `echo $t`")
-echo "screen_printer.printing_delay: $t" >> DTparameters.txt
+echo "screen_printer.printing_delay=$t" >> DTparameters.txt
 
 
 sh runEventQ.sh queries/screenprinter2Q.json
@@ -30,7 +30,9 @@ e=$(cat temp | jq '.aggregations.screen_printer_cleaning_energy.value')
 t=$(bc -l <<< "scale=1; $t/1")
 e=$(bc -l <<< "scale=1; $e/1")
 p2=$(bc -l <<< "scale=0;`echo $e` * 3600000 / `echo $t`")
-echo "screen_printer.cleaning_delay: $t" >> DTparameters.txt
+echo "screen_printer.cleaning_delay=$t" >> DTparameters.txt
+
+echo "screen_printer.set_power_ratings([$p0,$p0,$p1,$p2,$p0])" >> DTparameters.txt
 
 sh runEventQ.sh queries/pickandplace10Q.json
 t=$(cat temp | jq '.aggregations.pick_and_place_1_idle_delay.value')
@@ -38,7 +40,8 @@ e=$(cat temp | jq '.aggregations.pick_and_place_1_idle_energy.value')
 t=$(bc -l <<< "scale=1; $t/1")
 e=$(bc -l <<< "scale=1; $e/1")
 p0=$(bc -l <<< "scale=0;`echo $e` * 3600000 / `echo $t`")
-#echo "pick_and_place_1.idle_delay: $t" >> DTparameters.txt
+#echo "pick_and_place_1.idle_delay=$t" >> DTparameters.txt
+
 
 sh runEventQ.sh queries/pickandplace11Q.json
 t=$(cat temp | jq '.aggregations.pick_and_place_1_processing_delay.value')
@@ -46,7 +49,10 @@ e=$(cat temp | jq '.aggregations.pick_and_place_1_processing_energy.value')
 t=$(bc -l <<< "scale=1; $t/1")
 e=$(bc -l <<< "scale=1; $e/1")
 p0=$(bc -l <<< "scale=0;`echo $e` * 3600000 / `echo $t`")
-echo "pick_and_place_1.processing_delay: $t" >> DTparameters.txt
+echo "pick_and_place_1.processing_delay=$t" >> DTparameters.txt
+
+echo "pick_and_place_1.set_power_ratings([$p0,$p0,$p1,$p0])" >> DTparameters.txt
+
 
 sh runEventQ.sh queries/pickandplace20Q.json
 t=$(cat temp | jq '.aggregations.pick_and_place_2_idle_delay.value')
@@ -54,7 +60,7 @@ e=$(cat temp | jq '.aggregations.pick_and_place_2_idle_energy.value')
 t=$(bc -l <<< "scale=1; $t/1")
 e=$(bc -l <<< "scale=1; $e/1")
 p0=$(bc -l <<< "scale=0;`echo $e` * 3600000 / `echo $t`")
-#echo "pick_and_place_2.idle_delay: $t" >> DTparameters.txt
+#echo "pick_and_place_2.idle_delay=$t" >> DTparameters.txt
 
 sh runEventQ.sh queries/pickandplace21Q.json
 t=$(cat temp | jq '.aggregations.pick_and_place_2_processing_delay.value')
@@ -62,7 +68,9 @@ e=$(cat temp | jq '.aggregations.pick_and_place_2_processing_energy.value')
 t=$(bc -l <<< "scale=1; $t/1")
 e=$(bc -l <<< "scale=1; $e/1")
 p0=$(bc -l <<< "scale=0;`echo $e` * 3600000 / `echo $t`")
-echo "pick_and_place_2.processing_delay: $t" >> DTparameters.txt
+echo "pick_and_place_2.processing_delay=$t" >> DTparameters.txt
+
+echo "pick_and_place_2.set_power_ratings([$p0,$p0,$p1,$p0])" >> DTparameters.txt
 
 sh runStateQ.sh queries/reflowoven0Q.json
 t=$(cat temp | jq '.aggregations.reflowoven_off_delay.value')
@@ -85,7 +93,7 @@ t=$(bc -l <<< "scale=1; $t/1")
 e=$(bc -l <<< "scale=1; $e/1")
 p2=$(bc -l <<< "scale=0;`echo $e` * 3600000 / `echo $t`")
 
-
+echo "reflow_oven.set_power_ratings([$p0,$p2,$p1,$p1])" >> DTparameters.txt
 
 echo "Done.. Parameters are available in DTparameters.txt"
 
