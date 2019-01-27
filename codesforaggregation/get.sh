835949de01b73dada543424ce18e785cc303e9d7
iiot <iiotrbccps@gmail.com>
2019-01-27 08:41:53 +0000

    autocommit by richard

--- codesforaggregation/get.sh
+++ codesforaggregation/get.sh
@@ -1,1 +1,1 @@
-curl -XPOST 10.10.10.1:9200/streamingevent*/_search?pretty -H 'Content-type: application/json' -d @$1
+curl -XPOST 10.10.10.1:9200/streamingevent*/_search?size=10000 -H 'Content-type: application/json' -d @$1
