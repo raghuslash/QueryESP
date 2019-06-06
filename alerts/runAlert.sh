96b0d6c41a7b20397e73415e5e0d0fbca5385cfa
iiot <iiotrbccps@gmail.com>
2019-06-06 10:32:14 +0000

    added alerts

---
+++ alerts/runAlert.sh
@@ -0,0 +1,7 @@
+#echo "Got query : \n`cat $1`\n"
+
+curl -ss -H 'Content-Type: application/json' -XPOST 'vinyas:9200/streamingevents*/_search?pretty' -d @$1 -o tempB
+
+#cat tempB | jq .hits.total 
+
+
