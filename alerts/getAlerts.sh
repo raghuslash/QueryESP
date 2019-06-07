ebf19bccf3fe47c7ff14929c7233e0f234104f86
iiot <iiotrbccps@gmail.com>
2019-06-07 03:34:37 +0000

    using json

--- alerts/getAlerts.sh
+++ alerts/getAlerts.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
+echo "{" > alertParameters.json
+
+
 sh runAlert.sh alert_queries/sp
 sp_total=$(cat temp | jq .hits.total)
 solder_percent=$(bc -l <<< "scale=1; 100 - $sp_total%100")
-echo "screenprinter.solder=$solder_percent" > Alertparameters.txt
-
+echo "\"screenprinter_solder\":$solder_percent," >> alertParameters.json
 #sh runAlert.sh alert_queries/sp
 #sp_total=$(cat temp | jq .hits.total)
 service_in=$(bc -l <<< "scale=1; 30 - $sp_total%30")
-echo "screenprinter.service_in=$service_in" >> Alertparameters.txt
+echo "\"screenprinter_service_in\":$service_in," >> alertParameters.json
 
 sh runAlert.sh alert_queries/pp1
 pp1_total=$(cat temp | jq .hits.total)
 refill1=$(bc -l <<< "scale=1; (30 - $pp1_total%30)*100/30")
-echo "pickandplace1.refill1=$refill1" >> Alertparameters.txt
+echo "\"pickandplace1_refill1\":$refill1," >> alertParameters.json
 refill2=$(bc -l <<< "scale=1; (50 - $pp1_total%50)*100/50")
-echo "pickandplace1.refill2=$refill2" >> Alertparameters.txt
+echo "\"pickandplace1_refill2\":$refill2," >> alertParameters.json
 
 sh runAlert.sh alert_queries/pp2
 pp2_total=$(cat temp | jq .hits.total)
 refill1=$(bc -l <<< "scale=1; (30 - $pp2_total%30)*100/30")
-echo "pickandplace2.refill1=$refill1" >> Alertparameters.txt
+echo "\"pickandplace2_refill1\":$refill1," >> alertParameters.json
 refill2=$(bc -l <<< "scale=1; (50 - $pp2_total%50)*100/50")
-echo "pickandplace2.refill2=$refill2" >> Alertparameters.txt
+echo "\"pickandplace2_refill2\":$refill2" >> alertParameters.json
+
+echo "}" >> alertParameters.json
+
+cp alertParameters.json ../../Desktop/freeboard-alerts/
